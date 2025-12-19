14ac55eb1904d9221a5957c92022ab2ae5ac9c7f
kimmy-N <ayiakinashipai@gmail.com>
2025-12-19 16:18:40 +0300

    Refactor test helpers to include resetDOM function
    Removed JSDOM setup and related dependencies. Added resetDOM function to initialize the test environment.

--- test/helpers.js
+++ test/helpers.js
@@ -1,36 +1,17 @@
-const chai = require('chai');
-global.expect = chai.expect;
+function resetDOM() {
+  document.body.innerHTML = `
+    <h1>Lab: Event Listeners With JavaScript</h1>
+    <button id="changeColorButton">Change Background Color</button>
+    <button id="resetColorButton">Reset Background Color</button>
+    <p id="keyPressDisplay">Press any key...</p>
+    <input type="text" id="textInput" placeholder="Type something...">
+    <p id="textInputDisplay">Your input will be displayed here.</p>
+    <form id="sampleForm">
+      <input type="text" id="formInput" placeholder="Enter something to submit...">
+      <button type="submit">Submit</button>
+    </form>
+    <p id="formResult"></p>
+  `
+}
 
-const fs = require('fs');
-const path = require('path');
-const { JSDOM } = require('jsdom');
-const babel = require('@babel/core');
-
-// Load HTML content
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
-
-// Transform JavaScript using Babel
-const { code: transformedScript } = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'),
-  { presets: ['@babel/preset-env'] }
-);
-
-// Initialize JSDOM
-const dom = new JSDOM(html, {
-  runScripts: "dangerously",
-  resources: "usable"
-});
-
-// Inject the transformed JavaScript into the virtual DOM
-const scriptElement = dom.window.document.createElement("script");
-scriptElement.textContent = transformedScript;
-dom.window.document.body.appendChild(scriptElement);
-
-// Expose JSDOM globals to the testing environment
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = dom.window.navigator;
-global.HTMLElement = dom.window.HTMLElement;
-global.Node = dom.window.Node;
-global.Text = dom.window.Text;
-global.XMLHttpRequest = dom.window.XMLHttpRequest;
+module.exports = { resetDOM }
